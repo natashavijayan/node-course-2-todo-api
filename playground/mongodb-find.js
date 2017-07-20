0dc46b5adaa1865f05687dd63c4454c4b3e0931a
Natasha Vijayan <natashavijayan@gmail.com>
2017-07-20 11:20:41 +0530

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const{MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+/*  db.collection('Todos').find({
+    _id : new ObjectID('59703baa983893a3d26151dd')
+  }).toArray().then((docs) => {
+    console.log('Todos : ');
+    console.log(JSON.stringify(docs,undefined,2));
+  }, (err) => {
+    console.log('Unable to fetch todos',err);
+  });
+  //db.close(); */
+  db.collection('Users').find().count().then((count) => {
+      console.log(`Users count: ${count}`);
+
+    }, (err) => {
+      console.log('Unable to fetch users',err);
+    });
+
+    db.collection('Users').find({
+        name : 'natasha'
+      }).toArray().then((docs) => {
+        console.log('Users : ');
+        console.log(JSON.stringify(docs,undefined,2));
+      }, (err) => {
+        console.log('Unable to fetch users',err);
+      });
+});
